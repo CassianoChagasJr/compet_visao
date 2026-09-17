47ca636abd6f22eab607547d8d10582cda8d8451
CassianoChagasJr <cassianojrchagas@gmail.com>
2026-09-17 19:35:44 -0300

    Versão final do script

--- script_att_m1.py
+++ script_att_m1.py
@@ -2,10 +2,9 @@ import cv2
 import numpy as np
 
 # --- Seção de Criação de Vídeo ---
-# Configurações para a criação do vídeo (FPS, dimensões, nome do arquivo)
-fps_criacao = 20
-largura_criacao, altura_criacao = 320, 240
-nome_arquivo_video_criado = "video_atividade_m1.mp4"
+fps_criacao = 20 #FPS
+largura_criacao, altura_criacao = 320, 240 #Dimensões do video
+nome_arquivo_video_criado = "video_atividade_m1.mp4" #Nome do arquivo
 
 # Define o codec para o vídeo (MP4V)
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
@@ -21,10 +20,8 @@ escritor = cv2.VideoWriter(
 # Loop para gerar e adicionar quadros ao vídeo
 num_quadros_criacao = 60
 for i in range(num_quadros_criacao):
-    # Cria um quadro branco
-    quadro = np.full((altura_criacao, largura_criacao, 3), 255, dtype=np.uint8)
-    # Calcula a posição X do círculo para criar um movimento
-    x_pos = int(30 + (largura_criacao - 60) * i / num_quadros_criacao)
+    quadro = np.full((altura_criacao, largura_criacao, 3), 255, dtype=np.uint8) # Cria um quadro branco
+    x_pos = int(30 + (largura_criacao - 60) * i / num_quadros_criacao)  # Calcula a posição X do círculo para criar um movimento
 
     # Desenha um círculo azul no quadro
     cv2.circle(
@@ -67,20 +64,19 @@ else:
     # Libera o objeto de captura
     captura.release()
 
-    # Se quadros foram lidos, processa e exibe o quadro do meio
-    if quadros_lidos:
-        # Calcula o índice do quadro do meio
-        indice_quadro_meio = len(quadros_lidos) // 2
-        nome_arquivo_quadro_meio = 'Quadro_do_meio.jpg'
-        # Salva o quadro do meio como imagem
-        cv2.imwrite(nome_arquivo_quadro_meio, quadros_lidos[indice_quadro_meio])
+ # Calcula o índice do quadro do meio
+    indice_quadro_meio = len(quadros_lidos) // 2
+    nome_arquivo_quadro_meio = 'Quadro_do_meio.jpg'
+    cv2.imwrite(nome_arquivo_quadro_meio, quadros_lidos[indice_quadro_meio]) # Salva o quadro do meio como imagem
 
-        # Carrega e exibe o quadro do meio no Colab
-        quadro_meio_para_exibir = cv2.imread(nome_arquivo_quadro_meio)
-        print("\nQuadro do meio:")
-        cv2_imshow(quadro_meio_para_exibir)
-    else:
-        print("Nenhum quadro foi lido do vídeo para análise.")
+    # Carrega e exibe o quadro do meio
+    quadro_meio_para_exibir = cv2.imread(nome_arquivo_quadro_meio)
+    print("\nQuadro do meio:")
+
+    # Código para exibição
+    cv2.imshow('Quadro do Meio', quadro_meio_para_exibir)
+    cv2.waitKey(0) # Espera uma tecla ser pressionada
+    cv2.destroyAllWindows() # Fecha todas as janelas do OpenCV
 
     # Exibe o total de quadros lidos
     print(f'\nTotal de quadros lidos do vídeo: {len(quadros_lidos)}')
