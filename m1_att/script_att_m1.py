883934e116ffc2cba52696a28a7a78c113d8c966
CassianoChagasJr <cassianojrchagas@gmail.com>
2026-09-17 15:03:14 -0300

    Correção do script para atividade m1

--- m1_att/script_att_m1.py
+++ m1_att/script_att_m1.py
@@ -2,7 +2,9 @@ import cv2
 
 # Script que abre um video em formato mp4, conta quantos quadros tem e salva o quadro do meio
 
-cap = cv2.VideoCapture("Caminho para o vídeo .mp4") #caminho para pegar o video na maquina e fazer a analise e corte
+#caminho para pegar o video na maquina e fazer a analise e corte
+caminho = ""
+cap = cv2.VideoCapture(caminho)
 quadros = []
 
 while True:
@@ -11,16 +13,19 @@ while True:
         break
     quadros.append(quadro)
 
+
+# propriedades do video
+print("Propriedades do vídeo:")
+print(f"  Largura: {int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))}")
+print(f"  Altura: {int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))}")
+print(f"  FPS: {cap.get(cv2.CAP_PROP_FPS)}")
+# print(f"  Codec: {chr(int(cap.get(cv2.CAP_PROP_FOURCC) & 0xFF))}{chr(int(cap.get(cv2.CAP_PROP_FOURCC) >> 8 & 0xFF))}{chr(int(cap.get(cv2.CAP_PROP_FOURCC) >> 16 & 0xFF))}{chr(int(cap.get(cv2.CAP_PROP_FOURCC) >> 24 & 0xFF))}")
+print(f"  Contagem de quadros (estimada pelo codec): {int(cap.get(cv2.CAP_PROP_FRAME_COUNT))}")
+
 cap.release()
 
 quadro_do_meio = len(quadros) // 2
 
 cv2.imwrite('Quadro_do_meio.jpg', quadros[quadro_do_meio])
 
-print(f'Total dos quadros no video: {len(quadro)}')
-
-# propriedades do video
-cap.get()
-
-print(f"propriedades do video: {cap.get()}")
-
+print(f'Total dos quadros no video: {len(quadros)}')
